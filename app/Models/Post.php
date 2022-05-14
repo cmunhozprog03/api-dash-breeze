51878692b9936f27297e9a6b207f366e4d3ea7ba
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-14 11:47:23 -0300

    Ralção entre as tabelas

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,31 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable= ['name', 'slug', 'extract', 'body', 'status'];
+
     const BORRADOR = 1;
     const PUBLICADO = 2;
+
+    // one => many inverse
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // many => many
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // one => morph
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // one to many
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
